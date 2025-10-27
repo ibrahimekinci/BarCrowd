cf2c6893665520352fb564b44755d9fa63a275fc
Ibrahim Ekinci <ibrahimekinci36@gmail.com>
2025-10-27 13:26:00 +1100

    updated configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("com.android.library") version "8.13.0" apply false
 
-    // 1. Safe Args Plugin: Necessary for generating Java classes
-    // for type-safe navigation and argument passing.
-    id("androidx.navigation.safeargs") version "2.9.5" apply false
+    id("androidx.navigation.safeargs") version "2.9.5" apply false // for type-safe navigation and argument passing.
+    id("com.google.gms.google-services") version "4.4.4" apply false // Required for Firebase
 }
